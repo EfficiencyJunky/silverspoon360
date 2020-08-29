f2370d58e9715a42fb7c86cd55c3cc8111f67a82
Turner Kirk <voxpipes@gmail.com>
2020-08-29 11:00:14 -0700

    add background layer mask

--- js/settings.js
+++ js/settings.js
@@ -1,2 +1,14 @@
 const bpm = 145;
 const secondsPerBeat = 60/bpm;
+
+const beat = 1;
+const bar = beat*4;
+const line = bar*4;
+const line_half = line/2;
+const stanza = line*4;
+const stanza_half = stanza/2;
+
+const intro_leadin = 0;
+const intro_chorus = stanza_half;
+const verse_1 = stanza;
+const preChorus_1 = stanza + stanza_half;
